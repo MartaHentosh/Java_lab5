d0a50e014dc9ce60699b5d016548fcb6b6684033
MartaHentosh <marta25gentosh@gmail.com>
2023-05-01 20:32:37 +0300

    lab5

--- src/main/java/ua/lviv/iot/algo/part1/lab5/FindWords.java
+++ src/main/java/ua/lviv/iot/algo/part1/lab5/FindWords.java
@@ -3,13 +3,28 @@ package ua.lviv.iot.algo.part1.lab5;
 import java.util.HashSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-public class FindWords {
+
+public final class FindWords {
+
     private final String text;
     private final int length;
+    public static final String regex = "(\\s\\w{5}\\s)";
+    public static final Pattern pattern = Pattern.compile(regex);
+
     public FindWords(String text, int length) {
         this.text = text;
         this.length = length;
     }
+
+    public HashSet<String> findWordsWithLength() {
+        HashSet<String> someWords = new HashSet<>();
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            someWords.add(matcher.group());
+        }
+        return someWords;
+    }
+
     public static void main(String[] args) {
         if (args.length < 2) {
             System.out.println("Need: java Main <text> <length>");
@@ -21,14 +36,5 @@ public class FindWords {
         HashSet<String> result = findWords.findWordsWithLength();
         System.out.println(result);
     }
-    public HashSet<String> findWordsWithLength() {
-        String regex = "\\b\\w{" + length + "}\\b";
-        Pattern pattern = Pattern.compile(regex);
-        HashSet<String> someWords = new HashSet<>();
-        Matcher matcher = pattern.matcher(text);
-        while (matcher.find()) {
-            someWords.add(matcher.group());
-        }
-        return someWords;
-    }
+
 }
